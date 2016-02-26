a39af9553497cd71ce46cb2924eba6e48c6e3c88
Kevin Chou <kevchou@gmail.com>
2016-02-25 23:55:49 -0500

    added some osx tweaks

--- init.el
+++ init.el
@@ -5,7 +5,6 @@
 ;; UTF-8 as default encoding
 (set-language-environment "UTF-8")
 
-
 ;; This package lets you auto install a package if you dont have
 (require 'package)
 (package-initialize)
@@ -17,6 +16,13 @@
       (package-refresh-contents)
       (package-install 'use-package)))
 (require 'use-package)
+
+
+;; sets $MANPATH, $PATH and exec-path from your shell, but only on OS X.
+(use-package exec-path-from-shell
+  :ensure exec-path-from-shell)
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
 
 
 ;; Python - ELPY
@@ -33,6 +39,11 @@
             ))
           
 
+;; git gutter
+(use-package git-gutter-fringe+
+  :ensure git-gutter-fringe+)
+(global-git-gutter+-mode)
+
 ;; Emacs Speaks Statistics
 (use-package ess
   :ensure ess)
@@ -47,17 +58,6 @@
 
 (use-package auto-complete
   :ensure auto-complete)
-
-
-(use-package rainbow-delimiters
-  :ensure rainbow-delimiters)
-(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
-
-
-;; Evil mode for VIM keys
-;; (use-package eviil
-;;   :ensure evil)
-;; (evil-mode 1)
 
 
 ;; Multiple cursors like in Sublime text
@@ -88,92 +88,92 @@
 ;; Mode line modifications
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(use-package powerline
-  :ensure powerline)
-(powerline-default-theme)
+;; (use-package powerline
+;;   :ensure powerline)
+;; (powerline-default-theme)
 
-(defface powerline-custom1
-  '((t (:background "#EEAD0E" :foreground "black" :weight bold)))
-  "Custom face for bright sections"
-  :group 'powerline)
+;; (defface powerline-custom1
+;;   '((t (:background "#EEAD0E" :foreground "black" :weight bold)))
+;;   "Custom face for bright sections"
+;;   :group 'powerline)
 
-(defface powerline-custom2
-  '((t (:foreground "#EEAD0E" :weight bold)))
-  "Custom face for text"
-  :group 'powerline)
+;; (defface powerline-custom2
+;;   '((t (:foreground "#EEAD0E" :weight bold)))
+;;   "Custom face for text"
+;;   :group 'powerline)
 
-(defun my/anzu-update-func (here total)
-  "Customizing how anzu displays HERE & TOTAL on the mode line."
-  (propertize (format " <%d/%d>" here total)
-              'face 'powerline-custom1))
-(setq anzu-mode-line-update-function 'my/anzu-update-func)
+;; (defun my/anzu-update-func (here total)
+;;   "Customizing how anzu displays HERE & TOTAL on the mode line."
+;;   (propertize (format " <%d/%d>" here total)
+;;               'face 'powerline-custom1))
+;; (setq anzu-mode-line-update-function 'my/anzu-update-func)
 
 
-(defun powerline-spacemacs-imitation-theme ()
-  "An attempt to imitate the spacemacs powerline theme."
-  (interactive)
-  (setq-default mode-line-format
-                '("%e"
-                  (:eval
-                   (let* ((active (powerline-selected-window-active))
-                          (mode-line (if active 'mode-line 'mode-line-inactive))
-                          (face1 (if active 'powerline-active1 'powerline-inactive1))
-                          (face2 (if active 'powerline-active2 'powerline-inactive2))
-                          (face3 (if active 'powerline-custom1 mode-line))
-                          (face4 (if active 'powerline-custom2 mode-line))
-                          (separator-left (intern (format "powerline-%s-%s"
-                                                          (powerline-current-separator)
-                                                          (car powerline-default-separator-dir))))
-                          (separator-right (intern (format "powerline-%s-%s"
-                                                           (powerline-current-separator)
-                                                           (cdr powerline-default-separator-dir))))
-                          (lhs (list (powerline-raw "%*" face3 'l)
-                                     (powerline-raw " " face3)
-                                     (funcall separator-left face3 mode-line)
+;; (defun powerline-spacemacs-imitation-theme ()
+;;   "An attempt to imitate the spacemacs powerline theme."
+;;   (interactive)
+;;   (setq-default mode-line-format
+;;                 '("%e"
+;;                   (:eval
+;;                    (let* ((active (powerline-selected-window-active))
+;;                           (mode-line (if active 'mode-line 'mode-line-inactive))
+;;                           (face1 (if active 'powerline-active1 'powerline-inactive1))
+;;                           (face2 (if active 'powerline-active2 'powerline-inactive2))
+;;                           (face3 (if active 'powerline-custom1 mode-line))
+;;                           (face4 (if active 'powerline-custom2 mode-line))
+;;                           (separator-left (intern (format "powerline-%s-%s"
+;;                                                           (powerline-current-separator)
+;;                                                           (car powerline-default-separator-dir))))
+;;                           (separator-right (intern (format "powerline-%s-%s"
+;;                                                            (powerline-current-separator)
+;;                                                            (cdr powerline-default-separator-dir))))
+;;                           (lhs (list (powerline-raw "%*" face3 'l)
+;;                                      (powerline-raw " " face3)
+;;                                      (funcall separator-left face3 mode-line)
                                      
-                                     (when powerline-display-buffer-size
-                                       (powerline-buffer-size nil 'l))
-                                     (when powerline-display-mule-info
-                                       (powerline-raw mode-line-mule-info face4 'l))
-                                     (powerline-buffer-id face4 'l)
-                                     (when (and (boundp 'which-func-mode) which-func-mode)
-                                       (powerline-raw which-func-format nil 'l))
-                                     (powerline-raw " ")
-                                     (funcall separator-left mode-line face1)
+;;                                      (when powerline-display-buffer-size
+;;                                        (powerline-buffer-size nil 'l))
+;;                                      (when powerline-display-mule-info
+;;                                        (powerline-raw mode-line-mule-info face4 'l))
+;;                                      (powerline-buffer-id face4 'l)
+;;                                      (when (and (boundp 'which-func-mode) which-func-mode)
+;;                                        (powerline-raw which-func-format nil 'l))
+;;                                      (powerline-raw " ")
+;;                                      (funcall separator-left mode-line face1)
                                      
-                                     (when (boundp 'erc-modified-channels-object)
-                                       (powerline-raw erc-modified-channels-object face1 'l))
-                                     (powerline-major-mode face1 'l)
-                                     (powerline-process face1)
-                                     (powerline-raw " " face1)
-                                     (funcall separator-right face1 mode-line)
+;;                                      (when (boundp 'erc-modified-channels-object)
+;;                                        (powerline-raw erc-modified-channels-object face1 'l))
+;;                                      (powerline-major-mode face1 'l)
+;;                                      (powerline-process face1)
+;;                                      (powerline-raw " " face1)
+;;                                      (funcall separator-right face1 mode-line)
                                      
-                                     (powerline-minor-modes mode-line 'l)
-                                     (powerline-narrow mode-line 'l)
-                                     (powerline-raw " " mode-line)
-                                     (funcall separator-left mode-line face1)
+;;                                      (powerline-minor-modes mode-line 'l)
+;;                                      (powerline-narrow mode-line 'l)
+;;                                      (powerline-raw " " mode-line)
+;;                                      (funcall separator-left mode-line face1)
                                      
-                                     (powerline-vc face1 'r)
-                                     (powerline-raw " " face1)
-                                     (funcall separator-right face1 face2)
+;;                                      (powerline-vc face1 'r)
+;;                                      (powerline-raw " " face1)
+;;                                      (funcall separator-right face1 face2)
                                      
-                                     (when (bound-and-true-p nyan-mode)
-                                       (powerline-raw (list (nyan-create)) face2 'l))))
-                          (rhs (list (powerline-raw global-mode-string face2 'r)
-                                     (funcall separator-right face2 face1)
-                                     (unless window-system
-                                       (powerline-raw (char-to-string #xe0a1) face1 'l))
-                                     (powerline-raw "%4l" face1 'l)
-                                     (powerline-raw ":" face1 'l)
-                                     (powerline-raw "%3c" face1 'r)
-                                     (funcall separator-right face1 mode-line)
-                                     (powerline-raw " ")
-                                     (powerline-raw "%6p" nil 'r))))
-                     (concat (powerline-render lhs)
-                             (powerline-fill face2 (powerline-width rhs))
-                             (powerline-render rhs)))))))
+;;                                      (when (bound-and-true-p nyan-mode)
+;;                                        (powerline-raw (list (nyan-create)) face2 'l))))
+;;                           (rhs (list (powerline-raw global-mode-string face2 'r)
+;;                                      (funcall separator-right face2 face1)
+;;                                      (unless window-system
+;;                                        (powerline-raw (char-to-string #xe0a1) face1 'l))
+;;                                      (powerline-raw "%4l" face1 'l)
+;;                                      (powerline-raw ":" face1 'l)
+;;                                      (powerline-raw "%3c" face1 'r)
+;;                                      (funcall separator-right face1 mode-line)
+;;                                      (powerline-raw " ")
+;;                                      (powerline-raw "%6p" nil 'r))))
+;;                      (concat (powerline-render lhs)
+;;                              (powerline-fill face2 (powerline-width rhs))
+;;                              (powerline-render rhs)))))))
 
-(powerline-spacemacs-imitation-theme)
+;; (powerline-spacemacs-imitation-theme)
 
 
 ;; Add directory of file in the mode line
